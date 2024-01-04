cdcd6663240e06fa6a354072b89e024555188ea0
Ferdinand Bachmann <ferdinand.bachmann@yrlf.at>
2024-01-04 15:57:45 +0100

    linux: add linux kernel update warning module

--- waybar_custom_modules/cli.py
+++ waybar_custom_modules/cli.py
@@ -1,5 +1,24 @@
+import sys
+from . import linux
+
+def usage():
+    print("usage: waybar-custom-modules <module>")
+    print("")
+    print("MODULES:")
+    print("- linux: displays a warning if the kernel was updated")
+    sys.exit(1)
+
 def main():
-    pass
+    args = sys.argv[1:]
+
+    if len(args) != 1:
+        usage()
+
+    name = args[0]
+    if name == "linux":
+        linux.main()
+    else:
+        usage()
 
 if __name__ == "__main__":
     main()
---
+++ waybar_custom_modules/linux.py
@@ -0,0 +1,28 @@
+from subprocess import check_output
+import os.path
+import time
+import json
+import sys
+
+def report_empty():
+    print(json.dumps({
+        "text": "",
+        "percentage": 0,
+    }), flush=True)
+
+def report_updated(uname: str):
+    print(json.dumps({
+        "text": "",
+        "tooltip": uname,
+        "percentage": 100
+    }), flush=True)
+
+def main():
+    while True:
+        uname = check_output(["uname", "-r"]).decode()[:-1]
+        if os.path.exists(f"/usr/lib/modules/{uname}/vmlinuz"):
+            report_empty()
+        else:
+            report_updated(uname)
+
+        time.sleep(5)
